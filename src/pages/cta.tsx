9dcd712f5ffad2f0391ca7d03224cc5a55570ca8
apolorezende <apolo_henriquerr@hotmail.com>
2023-05-08 22:37:12 -0300

    feat: cta page refactored

--- src/pages/cta.tsx
+++ src/pages/cta.tsx
@@ -1,13 +1,13 @@
-// import de components NextJS
-import Link from "next/link";
 import Head from "next/head";
-import Image from "next/image";
-
-// import de components
 import { styled } from "../styles";
 import { HomeContainer } from "../styles/pages/home";
+
+import "keen-slider/keen-slider.min.css";
 import { ButtonCTA } from "../styles/buttons";
-import { TypographyH1 } from "../styles/typography";
+import { TypographyH1, TypographyP1 } from "../styles/typography";
+import { GlassMorphism, GlassNotification } from "../styles/pages/cta";
+import Image from "next/image";
+import logoNota from "../assets/logo.svg";
 import { NavbarMobile, UnorderedList, ListItems } from "../styles/navbar";
 
 // import de icones para navbar
@@ -16,9 +16,7 @@ import navSecondIcon from "../assets/mdi_map-check-outline.png";
 import navThirteenIcon from "../assets/material-symbols_check-circle-outline.png";
 import navFourIcon from "../assets/uil_setting.png";
 
-import "keen-slider/keen-slider.min.css";
-
-export default function Home() {
+export default function Cta() {
   return (
     <>
       <Head>
@@ -28,10 +26,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeContainer>
-        <TypographyH1>
-          Veja em quais universidades você seria aprovado!
-        </TypographyH1>
-        <ButtonCTA>Simular minha nota agora</ButtonCTA>
+        <GlassMorphism>
+          <TypographyH1>Parabéns!</TypographyH1>
+        </GlassMorphism>
+        <Image src={logoNota} alt="" height={200} width={200} />
+        <GlassNotification>
+          <TypographyP1>NOTA+</TypographyP1>
+          <TypographyP1>
+            Você acabou de ser aprovado em 30 universidades
+          </TypographyP1>
+        </GlassNotification>
+        <ButtonCTA
+          css={{
+            background:
+              "linear-gradient(90deg, #4FE35E 2.32%, #30E74D 107.7%);"
+          }}
+        >
+          Ver todas universidades
+        </ButtonCTA>
       </HomeContainer>
       <NavbarMobile>
         <UnorderedList>
