0c138459301b3a2b34854e2f08fb20f878528a7d
apolorezende <apolo_henriquerr@hotmail.com>
2023-05-08 22:36:25 -0300

    feat: navbar created

---
+++ src/styles/navbar.ts
@@ -0,0 +1,19 @@
+import { styled } from ".";
+
+export const NavbarMobile = styled("nav", {
+    width: "100%",
+    height: "50px",
+    textAlign: "center"
+})
+
+export const UnorderedList = styled("ul", {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: "100%",
+    padding: "0px 40px"
+});
+
+export const ListItems = styled("li", {
+    listStyle: "none",
+});
