2e66a57e2616b2aa3254050c5e02bd998e8aa5e3
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-10 00:12:56 -0300

    feat: config backend with first server api

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,7 +1,64 @@
-import "reflect-metadata";
+import express, { Request, Response } from "express";
+import { engProd } from "./utils/data";
 
-async function main() {
-  console.log("Sever running on ");
+const app = express();
+const port = 3001;
+
+function handleMediaPonderada(
+  LP: number,
+  LPpeso: number,
+  MAT: number,
+  MATpeso: number,
+  CH: number,
+  CHpeso: number,
+  CN: number,
+  CNpeso: number,
+  RED: number,
+  REDpeso: number
+) {
+  const mediaPonderada =
+    LP * LPpeso +
+    MAT * MATpeso +
+    CH * CHpeso +
+    CN * CNpeso +
+    (RED * REDpeso) / (LPpeso + MATpeso + CHpeso + LPpeso + CNpeso + REDpeso);
+
+  return mediaPonderada;
 }
 
-main();
+app.get("/engProd/:LP/:MAT/:CH/:CN/:RED", (req: Request, res: Response) => {
+  const { LP, MAT, CH, CN, RED } = req.params;
+  //logica aqui
+  const passou = true;
+  const result = [
+    {
+      nameCollege: "UFOP",
+      // @ts-ignore
+      mediaGeral: handleMediaPonderada(LP, 1, MAT, 3, CH, 2, CN, 2, RED, 1),
+      approved: passou,
+      LPnote: LP,
+      MATnote: MAT,
+      CHnote: CH,
+      CNnote: CN,
+      REDnote: RED,
+    },
+    {
+      nameCollege: "UFRJ",
+      approved: !passou,
+      LPnote: LP,
+      MATnote: MAT,
+      CHnote: CH,
+      CNnote: CN,
+      REDnote: RED,
+    },
+  ];
+  res.send(result);
+});
+
+app.get("/oi", (req: Request, res: Response) => {
+  res.send(`oiiii`);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ apps/backend/utils/data.ts
@@ -0,0 +1,11 @@
+export const engProd = [
+  {
+    UFOP: {
+      LP: 1,
+      MAT: 2,
+      CH: 3,
+      CN: 4,
+      RED: 5,
+    },
+  },
+];
