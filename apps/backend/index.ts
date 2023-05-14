f931b7f14b37f2320d877c32ce84324474ef67be
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-14 18:36:03 -0300

    feat: backend logic example

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -4,6 +4,8 @@ import { engProd } from "./utils/data";
 const app = express();
 const port = 3001;
 
+// precisa do Id curso, IDinstituicao
+// Criar collections para representar cursos, outra pra instituicoes e teremos uma model para as materias ( _id )
 function handleMediaPonderada(
   LP: number,
   LPpeso: number,
@@ -16,20 +18,18 @@ function handleMediaPonderada(
   RED: number,
   REDpeso: number
 ) {
-  const mediaPonderada =
-    LP * LPpeso +
-    MAT * MATpeso +
-    CH * CHpeso +
-    CN * CNpeso +
-    (RED * REDpeso) / (LPpeso + MATpeso + CHpeso + LPpeso + CNpeso + REDpeso);
+  const somaNotaPeso =
+    LP * LPpeso + MAT * MATpeso + CH * CHpeso + CN * CNpeso + RED * REDpeso;
+  const somaPeso = LPpeso + MATpeso + CHpeso + LPpeso + CNpeso + REDpeso;
+  const mediaPonderada = somaNotaPeso / somaPeso;
 
   return mediaPonderada;
 }
-
+//passar por body
 app.get("/engProd/:LP/:MAT/:CH/:CN/:RED", (req: Request, res: Response) => {
   const { LP, MAT, CH, CN, RED } = req.params;
   //logica aqui
-  const passou = true;
+
   const result = [
     {
       nameCollege: "UFOP",
@@ -46,7 +46,21 @@ app.get("/engProd/:LP/:MAT/:CH/:CN/:RED", (req: Request, res: Response) => {
         RED,
         engProd.UFOP.RED
       ),
-      approved: passou,
+      approved:
+        engProd.UFOP.notaCorte <
+        handleMediaPonderada(
+          // @ts-ignore
+          LP,
+          engProd.UFOP.LP,
+          MAT,
+          engProd.UFOP.MAT,
+          CH,
+          engProd.UFOP.CH,
+          CN,
+          engProd.UFOP.CN,
+          RED,
+          engProd.UFOP.RED
+        ),
       LPnote: LP,
       MATnote: MAT,
       CHnote: CH,
@@ -68,7 +82,21 @@ app.get("/engProd/:LP/:MAT/:CH/:CN/:RED", (req: Request, res: Response) => {
         RED,
         engProd.UFRJ.RED
       ),
-      approved: passou,
+      approved:
+        engProd.UFOP.notaCorte <
+        handleMediaPonderada(
+          // @ts-ignore
+          LP,
+          engProd.UFRJ.LP,
+          MAT,
+          engProd.UFRJ.MAT,
+          CH,
+          engProd.UFRJ.CH,
+          CN,
+          engProd.UFRJ.CN,
+          RED,
+          engProd.UFRJ.RED
+        ),
       LPnote: LP,
       MATnote: MAT,
       CHnote: CH,
@@ -77,10 +105,6 @@ app.get("/engProd/:LP/:MAT/:CH/:CN/:RED", (req: Request, res: Response) => {
     },
   ];
   res.send(result);
-});
-
-app.get("/oi", (req: Request, res: Response) => {
-  res.send(`oiiii`);
 });
 
 app.listen(port, () => {
