d0bd7307f68f4ddbc7356c00e60662488839ca58
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-14 18:36:12 -0300

    feat: fakeData example

--- apps/backend/utils/data.ts
+++ apps/backend/utils/data.ts
@@ -5,12 +5,14 @@ export const engProd = {
     CH: 2,
     CN: 2,
     RED: 1,
+    notaCorte: 650,
   },
   UFRJ: {
-    LP: 1,
+    LP: 2,
     MAT: 3,
-    CH: 2,
-    CN: 2,
-    RED: 1,
+    CH: 1,
+    CN: 1,
+    RED: 2,
+    notaCorte: 780,
   },
 };
