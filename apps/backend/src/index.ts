99ed86b7ee392010d2fb85eb256c16101563affa
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-16 21:38:34 -0300

    feat: connect with mongoDB repository

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,19 +3,23 @@ import express, { response } from "express";
 import { config } from "dotenv";
 import { MongoGetLeadsRepository } from "./repositories/get-leads/mongo-get-leads";
 import { GetLeadsController } from "./controllers/get-leads/get-leads";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
+  await MongoClient.connect();
+  const app = express();
 
-const app = express();
-const port = process.env.PORT || 8000;
+  app.get("/leads", async (req, res) => {
+    const mongoGetLeadsRepository = new MongoGetLeadsRepository();
 
-app.get("/leads", async (req, res) => {
-  const mongoGetLeadsRepository = new MongoGetLeadsRepository();
+    const getLeadsController = new GetLeadsController(mongoGetLeadsRepository);
+    const { body, statusCode } = await getLeadsController.handle();
 
-  const getLeadsController = new GetLeadsController(mongoGetLeadsRepository);
-  const { body, statusCode } = await getLeadsController.handle();
+    res.send(body).status(statusCode);
+  });
+  const port = process.env.PORT || 8000;
 
-  res.send(body).status(statusCode);
-});
-
-app.listen(port, () => console.log(`listening on port ${port}!`));
+  app.listen(port, () => console.log(`listening on port ${port}!`));
+};
+main();
